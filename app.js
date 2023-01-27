a7c9fd609d276326c0770278395880b9dbc786f9
Tapio Heiskanen <59442255+oh2th@users.noreply.github.com>
2023-01-27 09:36:15 +0200

    Add: New card - Alarm NN minutes ahead of time

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const Homey = require('homey');
 const { HomeyAPIApp } = require('homey-api');
+const { strToMins, minsToStr } = require('./lib/helpers');
 
 const INTERVAL = 5000;
 
@@ -28,7 +29,10 @@ class AlarmUtils extends Homey.App {
   }
 
   async initFlowActions() {
-    // Action: Set alarm time
+    //
+    // Action Flow Card: Alarm time
+    // Set selected alarms time from string args.alarmtime
+    //
     const action_set_alarm_time = this.homey.flow.getActionCard('action_set_alarm_time');
     // Return a list of alarms for the flow card argument autocomplete.
     action_set_alarm_time.registerArgumentAutocompleteListener('alarm', async (query, args) => {
@@ -46,7 +50,52 @@ class AlarmUtils extends Homey.App {
           time: args.alarmtime,
         },
       });
-      this.log(`${this.myAppIdVersion} - Action - action_set_alarm_time: "${args.alarm.name}" to "${args.alarmtime}"`);
+      this.homey.app.log(`${this.myAppIdVersion} - Action - action_set_alarm_time: "${args.alarm.name}" to "${args.alarmtime}"`);
+    });
+
+    //
+    // Action Flow Card: Set alarm ahead time (action_set_alarm_ahead_time)
+    // Sets the time for an alarm in local time as HH:mm string. If any weekday is selected, the alarm will be repeated on those weekdays. If no weekday is selected, the alarm will run once.
+    //
+    const action_set_alarm_ahead_time = this.homey.flow.getActionCard('action_set_alarm_ahead_time');
+    // Return a list of alarms for the flow card argument autocomplete.
+    action_set_alarm_ahead_time.registerArgumentAutocompleteListener('alarm', async (query, args) => {
+      const _alarms = Object.values(this.alarms);
+      return _alarms.filter((_alarm) => {
+        return _alarm.name.toLowerCase().includes(query.toLowerCase());
+      });
+    });
+    // Run the action.
+    action_set_alarm_ahead_time.registerRunListener(async (args, state) => {
+      this.log(`${this.myAppIdVersion} - Action - action_set_alarm_ahead_time: Requested "${args.alarm.name}" to "${args.minutes} mins before ${args.time}."`);
+      // Grab the weekdays from the flow card arguments and create a repetition object for updateAlarm.
+      const repetition = {
+        monday: args.monday, tuesday: args.tuesday, wednesday: args.wednesday, thursday: args.thursday, friday: args.friday, saturday: args.saturday, sunday: args.sunday,
+      };
+      let alarmTime = strToMins(args.time) - args.minutes;
+      // Handle negative time meaning minutes before midnight and set alarm time to previous day.
+      if (alarmTime < 0) {
+        alarmTime = 1440 + alarmTime;
+        repetition.monday = args.tuesday;
+        repetition.tuesday = args.wednesday;
+        repetition.wednesday = args.thursday;
+        repetition.thursday = args.friday;
+        repetition.friday = args.saturday;
+        repetition.saturday = args.sunday;
+        repetition.sunday = args.monday;
+      }
+      this.log(`${this.myAppIdVersion} - Action - action_set_alarm_ahead_time: "${args.alarm.name}" to "${minsToStr(alarmTime)} (${alarmTime} minutes)" on "${JSON.stringify(repetition)}."`);
+      try {
+        await this.api.alarms.updateAlarm({
+          id: args.alarm.id,
+          alarm: {
+            time: minsToStr(alarmTime),
+            repetition,
+          },
+        });
+      } catch (error) {
+        this.log(`${this.myAppIdVersion} - Action - action_set_alarm_ahead_time - Error: ${error}`);
+      }
     });
   }
 
