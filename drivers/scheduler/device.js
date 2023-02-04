c6555c96f6be2a55326a0ac09619501699a10cea
Tapio Heiskanen <59442255+oh2th@users.noreply.github.com>
2023-02-04 10:55:23 +0200

    onSettings and re-init cronJob with newSettings

--- drivers/scheduler/device.js
+++ drivers/scheduler/device.js
@@ -2,15 +2,19 @@
 
 const { Device } = require('homey');
 // CronJob from https://github.com/kelektiv/node-cron
-const { CronJob } = require('cron');
+const { CronJob, CronTime } = require('cron');
 
 module.exports = class SchedulerDevice extends Device {
 
   async onInit() {
     this.log(`${this.getName()} - init`);
-    const { cronTime, runOnce, timeZone } = await this.getSettingsCronTime();
-    this.initCronJob(cronTime, runOnce, timeZone);
+
+    const settings = this.getSettings();
+    const { cronTime, timeZone, runOnce } = this.getSettingsCronTime(settings);
+    this.initCronJob(cronTime, timeZone, runOnce);
+
     this.initCapabilityListeners();
+
     this.log(`${this.getName()} - init done`);
   }
 
@@ -19,7 +23,16 @@ module.exports = class SchedulerDevice extends Device {
   }
 
   async onSettings({ oldSettings, newSettings, changedKeys }) {
-    this.log(`${this.getName()} - settings where changed`);
+    this.log(`${this.getName()} - onSettings - changedKeys: ${changedKeys}`);
+
+    if (typeof this.cronJob !== 'undefined') {
+      this.log(`${this.getName()} - onSettings - stopping cronjob`);
+      this.cronJob.stop();
+      this.cronJob = undefined;
+    }
+
+    const { cronTime, timeZone, runOnce } = this.getSettingsCronTime(newSettings);
+    this.initCronJob(cronTime, timeZone, runOnce);
   }
 
   async onRenamed(name) {
@@ -31,23 +44,34 @@ module.exports = class SchedulerDevice extends Device {
     this.log(`${this.getName()} - has been deleted`);
   }
 
-  async getSettingsCronTime() {
-    const settings = this.getSettings();
-
+  /**
+   * @description Get the cronTime string from the settings
+   *
+   * @param {Object} settings The settings object
+   * @returns {Object} cronTime, runOnce, timeZone
+   * @example
+   * const { cronTime, runOnce, timeZone } = await this.getSettingsCronTime();
+   * this.initCronJob(cronTime, runOnce, timeZone);
+   * // cronTime: '0 0 12 * * 1-5'
+   * // timeZone: 'Europe/Helsinki'
+   * // runOnce: false - if any weekday is selected, runOnce will be false
+   */
+  getSettingsCronTime(settings) {
     const hours = settings.time.split(':')[0];
     const minutes = settings.time.split(':')[1];
     let runOnce = false;
     let weekdays = [
-      settings.repeat_sundays ? '0' : null,
-      settings.repeat_mondays ? '1' : null,
-      settings.repeat_tuesdays ? '2' : null,
-      settings.repeat_wednesdays ? '3' : null,
-      settings.repeat_thursdays ? '4' : null,
-      settings.repeat_fridays ? '5' : null,
-      settings.repeat_saturdays ? '6' : null,
+      settings.repeat_sunday ? '0' : null,
+      settings.repeat_monday ? '1' : null,
+      settings.repeat_tuesday ? '2' : null,
+      settings.repeat_wednesday ? '3' : null,
+      settings.repeat_thursday ? '4' : null,
+      settings.repeat_friday ? '5' : null,
+      settings.repeat_saturday ? '6' : null,
     ].filter((element) => {
       return element;
     }).join(',');
+
     if (weekdays === '') {
       weekdays = '*';
       runOnce = true;
@@ -55,19 +79,27 @@ module.exports = class SchedulerDevice extends Device {
       weekdays = '*';
       runOnce = false;
     }
-    // const cronTime = `0 ${minutes} ${hours} * * ${weekdays}`;
-    const cronTime = '0 * * * * *';
+
+    const cronTime = `0 ${minutes} ${hours} * * ${weekdays}`;
     const timeZone = settings.timezone;
-    return { cronTime, runOnce, timeZone };
+    return { cronTime, timeZone, runOnce };
   }
 
-  async initCronJob(cronTime, runOnce, timeZone) {
-    this.log(`${this.getName()} - initCronjob - cronTime: ${cronTime}, runOnce: ${runOnce}, timeZone: ${timeZone}`);
+  /**
+   * @description Initialize cronjob from saved settings
+   *
+   * @param {String} cronTime The cronTime string '0 0 12 * * 1-5' for example
+   * @param {String} timeZone Select the timezone for the cronjob
+   * @param {Boolean} runOnce If true, the cronjob will be disabled after the first run
+   */
+  async initCronJob(cronTime, timeZone, runOnce) {
+    this.log(`${this.getName()} - initCronjob - cronTime: ${cronTime}, timeZone: ${timeZone}, runOnce: ${runOnce}`);
 
+    // create cronjob with cronTime and timeZone, do not start yet
     this.cronJob = new CronJob({
       cronTime,
       onTick: () => {
-        this.log(`${this.getName()} - cron tick at ${new Date().toISOString()}`);
+        this.log(`${this.getName()} - cronJob - tick at ${new Date().toISOString()}`);
         this.runTriggers();
       },
       start: false,
@@ -76,11 +108,14 @@ module.exports = class SchedulerDevice extends Device {
 
     // add callback to cronjob to enable/disable cronjob on runOnce
     this.cronJob.addCallback(() => {
-      this.log(`${this.getName()} - cron callback`);
+      this.log(`${this.getName()} - cronJob - callback`);
       if (runOnce) {
         this.setCapabilityValue('is_enabled', false);
         this.cronJob.stop();
+        this.log(`${this.getName()} - cronJob - runOnce - not running again`);
+        return;
       }
+      this.log(`${this.getName()} - cronJob - next tick at ${this.cronJob.nextDates(1)}`);
     });
 
     // start cronjob if enabled
