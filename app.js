f66e6dd5e1bb6a9edb2bfa7ef62af0634bcdf3a0
Tapio Heiskanen <59442255+oh2th@users.noreply.github.com>
2023-03-03 08:12:09 +0200

    Start deprecating Homey Alarms support

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const { HomeyAPIApp } = require('homey-api');
 const { strToMins, minsToStr } = require('./lib/helpers');
 const flowActions = require('./lib/flows/actions');
 const flowConditions = require('./lib/flows/conditions');
-const flowTriggers = require('./lib/flows/triggers');
+const appFlowActions = require('./lib/app-flows/actions');
+const appFlowActions2 = require('./lib/app-flows/actions-platform2');
+const appFlowTriggers = require('./lib/app-flows/triggers');
 
 const INTERVAL = 5000;
 
@@ -15,42 +17,34 @@ class AlarmUtils extends Homey.App {
     this.myAppIdVersion = `${this.homey.manifest.id}/${this.homey.manifest.version}`;
     this.log(`${this.myAppIdVersion} - onInit - starting...`);
 
-    this.api = new HomeyAPIApp({
-      homey: this.homey,
-      debug: true,
-    });
-
-    await this.api.alarms.connect();
-    this.alarms = await this.api.alarms.getAlarms();
-    this.log(`${this.myAppIdVersion} - Found alarms: '${JSON.stringify(this.alarms, null, 2)}'`);
-
+    // Init device flow cards.
     await flowActions.init(this);
     await flowConditions.init(this);
-    await flowTriggers.init(this);
+
+    // Init Homey Alarms API and related flow cards, if on pre-Homey Pro 2023 platform.
+    if (this.homey.platform === 'local' && (this.homey.platformVersion === 1 || this.homey.platformVersion === undefined)) {
+      this.log(`${this.myAppIdVersion} - onInit - local platform version 1 or undefined - Homey-API and related app flow cards.`);
+
+      this.api = new HomeyAPIApp({
+        homey: this.homey,
+        debug: true,
+      });
+
+      await this.api.alarms.connect();
+      this.alarms = await this.api.alarms.getAlarms();
+      this.log(`${this.myAppIdVersion} - Found alarms: '${JSON.stringify(this.alarms, null, 2)}'`);
+
+      await appFlowActions.init(this);
+      await appFlowTriggers.init(this);
+    } else {
+      this.log(`${this.myAppIdVersion} - onInit - local platform version 2 or higher - Homey-API and related app flow cards not supported.`);
+      // Init dummy flow cards for Homey Pro 2023 platform to inform user to move to use the Scheduler device instead.
+      await appFlowActions2.init(this);
+    }
+
+    this.sendNotifications();
 
     this.log(`${this.myAppIdVersion} - onInit - started.`);
-  }
-
-  async checkAlarmAPIConnection() {
-    try {
-      if (!this.api.alarms.isConnected()) {
-        this.log(`${this.myAppIdVersion} - checkAlarmAPIConnection - reconnecting.`);
-        await this.api.alarms.connect();
-        await this.clearIntervals();
-        await this.setCheckAlarmAPIInterval();
-      }
-    } catch (error) {
-      this.log(`${this.myAppIdVersion} - checkAlarmAPIConnection - Error: '${error}'`);
-    }
-  }
-
-  async setCheckAlarmAPIInterval() {
-    try {
-      this.log(`${this.myAppIdVersion} - setInterval => ${INTERVAL}ms.`);
-      this.onPollInterval = setInterval(this.checkAlarmAPIConnection.bind(this), INTERVAL);
-    } catch (error) {
-      this.log(`${this.myAppIdVersion} - checkAlarmAPIConnection - Error: '${error}'`);
-    }
   }
 
   async clearIntervals() {
@@ -58,7 +52,39 @@ class AlarmUtils extends Homey.App {
       this.log(`${this.myAppIdVersion} - clearIntervals.`);
       clearInterval(this.onPollInterval);
     } catch (error) {
-      this.log(`${this.myAppIdVersion} - checkAlarmAPIConnection - Error: '${error}'`);
+      this.log(`${this.myAppIdVersion} - clearIntervals - Error: '${error}'`);
+    }
+  }
+
+  async sendNotifications() {
+    const ntfy_deprecation_01 = '[Action Scheduler] (1/2) - Support for the Homey Alarms API will be removed in a future version of this app.';
+    const ntfy_deprecation_02 = '[Action Scheduler] (2/2) - Please migrate your flows to use the included Scheduler devices instead of the Homey Alarms.';
+    const ntfy_deprecation_03 = '[Action Scheduler] (3/3) - Homey Pro 2023 devices will not support the Homey Alarms API.';
+
+    await this.homey.notifications.createNotification({
+      excerpt: ntfy_deprecation_01,
+    }).then((result) => {
+      console.log(result);
+    }).catch((err) => {
+      console.error(err);
+    });
+
+    await this.homey.notifications.createNotification({
+      excerpt: ntfy_deprecation_02,
+    }).then((result) => {
+      console.log(result);
+    }).catch((err) => {
+      console.error(err);
+    });
+
+    if (this.homey.platform === 'local' && this.homey.platformVersion === 2) {
+      await this.homey.notifications.createNotification({
+        excerpt: ntfy_deprecation_03,
+      }).then((result) => {
+        console.log(result);
+      }).catch((err) => {
+        console.error(err);
+      });
     }
   }
 
