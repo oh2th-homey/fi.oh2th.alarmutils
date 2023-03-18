702fd5f3760789dba9ed58322c25696e9ae0edf3
Tapio Heiskanen <59442255+oh2th@users.noreply.github.com>
2023-03-18 17:41:18 +0200

    Add trigger card for Scheduled time is in N min. - Add onAppInterval for every minute maintained from cron.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { HomeyAPIApp } = require('homey-api');
 const { CronJob } = require('cron');
 const flowActions = require('./lib/flows/actions');
 const flowConditions = require('./lib/flows/conditions');
+const flowTriggers = require('./lib/flows/triggers');
 const appFlowActions = require('./lib/app-flows/actions');
 const appFlowActions2 = require('./lib/app-flows/actions-platform2');
 const appFlowTriggers = require('./lib/app-flows/triggers');
@@ -19,6 +20,7 @@ class AlarmUtils extends Homey.App {
     // Init device flow cards.
     await flowActions.init(this);
     await flowConditions.init(this);
+    await flowTriggers.init(this);
 
     // Init Homey Alarms API and related flow cards, if on pre-Homey Pro 2023 platform.
     if (this.homey.platform === 'local' && (this.homey.platformVersion === 1 || this.homey.platformVersion === undefined)) {
@@ -61,15 +63,7 @@ class AlarmUtils extends Homey.App {
     this.appCronJob = new CronJob({
       cronTime: '0 * * * * *',
       onTick: () => {
-        this.log(`${this.myAppIdVersion} - onAppInterval`);
-
-        // Get all configured scheduler devices.
-        const schedulerDriver = this.homey.drivers.getDriver('scheduler');
-        const schedulerDevices = schedulerDriver.getDevices();
-        // Iterate over all scheduler devices and check if any of them should be triggered.
-        schedulerDevices.forEach((schedulerDevice) => {
-          this.log(`${this.myAppIdVersion} - Checking scheduler device: ${schedulerDevice.getName()}`);
-        });
+        this.onAppInterval();
       },
       start: false,
       // timeZone: 'Europe/Amsterdam',
@@ -77,12 +71,47 @@ class AlarmUtils extends Homey.App {
     this.appCronJob.start();
   }
 
-  onAppInterval() {
+  async onAppInterval() {
+    // this.log(`${this.myAppIdVersion} - onAppInterval`);
+
+    // Get all configured scheduler devices.
+    const schedulerDriver = this.homey.drivers.getDriver('scheduler');
+    const schedulerDevices = schedulerDriver.getDevices();
+
+    // Iterate over all active scheduler devices.
+    schedulerDevices.forEach((schedulerDevice) => {
+      if (schedulerDevice.getCapabilityValue('is_enabled') === true) {
+        const dateTimeNow = new Date();
+
+        const nextScheduledTime = schedulerDevice.getNextScheduledTime();
+        const dateTimeNext = new Date(nextScheduledTime);
+
+        const scheduledTime = dateTimeNext.toLocaleString('en-US', {
+          hour: '2-digit', minute: '2-digit', hour12: false, timeZone: this.homey.clock.getTimezone(),
+        });
+
+        const state = { minutes: Math.abs(Math.floor((dateTimeNext - dateTimeNow) / 60000)) + 1 };
+
+        const tokens = {
+          name: schedulerDevice.getName(),
+          time: scheduledTime,
+          next: nextScheduledTime,
+          minutesToNext: state.minutes,
+        };
+
+        // If next trigger in in the future trigger the device_schedule_trigger_in flow card.
+        if (dateTimeNext > dateTimeNow) {
+          this.homey.flow.getDeviceTriggerCard('device_schedule_trigger_in')
+            .trigger(schedulerDevice, tokens, state)
+            .catch(this.error);
+        }
+      }
+    });
   }
 
   async sendNotifications() {
     const ntfy_deprecation_01 = '[Action Scheduler] (1/2) - Support for the Homey Alarms API will be removed in a future version of this app.';
-    const ntfy_deprecation_02 = '[Action Scheduler] (2/2) - Please migrate your flows to use the included Scheduler devices instead of the Homey Alarms.';
+    const ntfy_deprecation_02 = '[Action Scheduler] (2/2) - Please migrate your flows to use the included Scheduler device instead.';
 
     await this.homey.notifications.createNotification({
       excerpt: ntfy_deprecation_01,
